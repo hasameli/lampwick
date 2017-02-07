9d5769d7efc910e26cae70560f9799e59cfc8e35
Dr-Syn <munin@hasameli.com>
2017-02-06 17:55:09 -0800

    flag order

--- lampwick.sh
+++ lampwick.sh
@@ -8,8 +8,8 @@ FILENAME=sitestoclone.txt
 
 
 # This first wget grabs the URLs listed in sitestoclone.txt and archives them locally.
-wget --limit-rate=40k --random-wait -mkKpr -l3 -np -nc -E -t 3 -i -P ./archive/ -U="Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)" -N $FILENAME
+wget --limit-rate=40k --random-wait -mkKpr -l3 -np -nc -E -t 3 -P ./archive/ -U="Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)" -N -i $FILENAME
 
 # This second wget grabs the links from the top level of the above URLs and pulls them down too.
-wget --limit-rate=40k --random-wait -r -k -K -H -N -l 1 -P ./archive/ -N $FILENAME
+wget --limit-rate=40k --random-wait -r -k -K -H -N -l 1 -P ./archive/ -N -i $FILENAME
 
