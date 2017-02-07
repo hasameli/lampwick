10b71d88e4409a2e5d8521bf814e2ea1cd705233
Dr-Syn <munin@hasameli.com>
2017-02-06 18:30:12 -0800

    Have this identify as googlebot too because we want the stripped pages served

--- lampwick.sh
+++ lampwick.sh
@@ -11,7 +11,7 @@ FILENAME=sitestoclone.txt
 wget --limit-rate=40k --random-wait -mkKpr -l3 -np -nc -E -t 3 -P ./archive/ -U="Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)" -N -i $FILENAME
 
 # This second wget grabs the links from the top level of the above URLs and pulls them down too.
-wget --limit-rate=40k --random-wait -r -k -K -H -N -l 1 -P ./archive/ -N -i $FILENAME
+wget --limit-rate=40k --random-wait -r -k -K -H -N -l 1 -P ./archive/ -N -U="Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)" -i $FILENAME
 
 # Now we change directory to the archive
 cd ./archive
